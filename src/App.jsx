655343a5144a0fb65f5fa460402cabb3e2521024
hossMurad <hoss.murad@gmail.com>
2025-03-28 22:14:50 +0600

    object in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import {useState} from "react";
+
 
 const App = () => {
+
+const [myObject , setMyObject] = useState({
+    name:"Md. Murad Hossain",
+    age: 23,
+    city:"Beijin",
+    home: "Dhaka"
+});
     return (
         <div>
-            
+            <h1>Name : {myObject.name}</h1>
+            <h1>age : {myObject.age}</h1>
+            <h1>city : {myObject.city}</h1>
+            <h1>home : {myObject.home}</h1>
+
         </div>
     );
 };
